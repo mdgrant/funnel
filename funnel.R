49610a378d9725476980616e96c9cda8797a0973
md grant <mdgrantmd@gmail.com>
2015-03-01 08:19:30 -0600

    added base plot

--- funnel.R
+++ funnel.R
@@ -1,14 +1,14 @@
 # 2015-02-27 heart transplant funnel plot adults 30d mortality
 
-
 ## code: download data, select variables for analysis ####
 # check if _data directory exists, create if needed
 if (!file.exists("_data")) {dir.create("_data")}
 
 # download and read data
+if (!file.exists("_data/heartTables.zip")){
 fileUrl <- "http://www.srtr.org/csr/archives/201406/csrs_final_tables201406HR.zip"
 download.file(fileUrl, destfile="_data/heartTables.zip", method="curl")
-unzip("_data/heartTables.zip", exdir="_data")
+unzip("_data/heartTables.zip", exdir="_data")}
 
 # read table 10 contains observed and expected
 fname <- '_data/csrs_final_tables201406HR.xls'
@@ -41,7 +41,8 @@ hrtAd <- hrtAd[hrtAd$ad_n_c1m > 2, ]
 ## code: data ####
 
 # calculate bounds, "Exact" 95% Confidence Intervals,  (qchisq(α/2, 2*x)/2, qchisq(1-α/2, 2*(x+1))/2 )
-# Ulm K. A simple method to calculate the confidence interval of a standardized mortality ratio. American Journal of Epidemiology 1990;131(2):373-375.
+# Ulm K. A simple method to calculate the confidence interval of a standardized mortality ratio. 
+# American Journal of Epidemiology 1990;131(2):373-375.
 
 # lower limits as %
 low2alpha <- -(1 - qchisq(0.025, 2*(c(1:10)))/2/c(1:10))*100
@@ -69,13 +70,13 @@ hrtAd$ests <- with(hrtAd, (ad_obs_c1m - ad_exp_c1m)/ad_exp_c1m * 100 )
 # * (end)
 
 
-## code: create plot ####
+## code: create plot ggplot ####
 library(ggplot2)
 library(scales)  
 
 adult30d <- ggplot(data=hrtAd, aes(x=ad_exp_c1m, y=ad_smr_c1m)) +
     geom_point(origin=0)  +
-    labs(x = "Expected Deaths", y = "SMR") +
+    labs(x = "Expected Deaths at 30 Days", y = "SMR") +
     stat_smooth(data=limits, aes(events, low2smr), size=1, method="loess", se = FALSE) +
     stat_smooth(data=limits, aes(events, low3smr), size=1, method="loess", se = FALSE, colour="red") +
     stat_smooth(data=limits, aes(events, up2smr),  size=1, method="loess", se = FALSE) +
@@ -88,7 +89,24 @@ ggsave("adult30d.pdf", width=5, height=5)
 # * (end)
 
 
+## code: create plot base graphics ####
+adult30dBase <- function(){
+par(bty="n")
+with(hrtAd, plot(ad_exp_c1m, sqrt(ad_smr_c1m), yaxt="n", ylab="SMR", xlab="Expected Deaths at 30 Days", ylim=c(0, sqrt(8))))
+axis(side=2, at=sapply(c(0:8), FUN=function(x) sqrt(x)), labels=c(0:8)) 
+with(limits, xspline(events, sqrt(low2smr), shape=1, lty=2))
+with(limits, xspline(events, sqrt(low3smr), shape=1, lty=3))
+with(limits, xspline(events, sqrt(up2smr),  shape=1, lty=2))
+with(limits, xspline(events, sqrt(up3smr),  shape=1, lty=3))
+abline(1, 0, col="gray")}
 
+adult30dBase()
+
+pdf("adult30dBase.pdf", width=5, height=5)
+adult30dBase()
+dev.off()
+
+# * (end)
 
 
 
